89ccfee4fba48e77e2c67a15c804992ef868d8c3
Salamander <113489812+salamandr1304@users.noreply.github.com>
2022-12-18 18:35:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let month = prompt("What month were you born? (capitalize your answer) ");
+switch(month){
+  case "January":
+    console.log("You are an Aquarius.")
+    break;
+  case "February":
+    console.log("You are a Pisces.")
+    break;
+  case "March":
+    console.log("You are an Aries.")
+    break;
+  case "April":
+    console.log("You are a Taurus.")
+    break;
+  case "May":
+    console.log("You are a Gemini.")
+    break;
+  case "June":
+    console.log("You are a Cancer.")
+    break;
+  case "July":
+    console.log("You are a Leo.")
+    break;
+  case "August":
+    console.log("You are a Virgo.")
+    break;
+  case "September":
+    console.log("You are a Libra.")
+    break;
+  case "October":
+    console.log("You are a Scorpio.")
+    break;
+  case "November":
+    console.log("You are a Saggitarius.")
+    break;
+  case "December":
+    console.log("You are a Capricorn.")
+    break;
+  default:
+    console.log("This is not a month.")
+    break;
+}
